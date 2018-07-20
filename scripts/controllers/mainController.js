514db5254b47410c55c403faf8503459f34da7ed
Jeet <Jeet@Metapercept.com>
2018-07-20 15:36:31 +0530

    0.0.6

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -1,4 +1,8 @@
 appName.controller('mainController', function ($scope) {
 
+	// Display the current version
+	var version = require('electron').remote.app.getVersion();
+	console.log("v" + version);
+	document.getElementById('version').innerText = version;	
 
 });
