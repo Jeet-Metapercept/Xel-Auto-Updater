7103ddf75f3ab8b6900d80697dba66c8002eb0c5
Jeet <Jeet@Metapercept.com>
2018-07-23 19:16:43 +0530

    0.4.7

--- assets/scripts/controllers/loginController.js
+++ assets/scripts/controllers/loginController.js
@@ -1,4 +1,25 @@
 appName.controller('loginController', function ($scope) {
 
+/* const ipcRenderer = require('electron').ipcRenderer;
+const {autoUpdater} = require("electron-updater");
+
+	autoUpdater.on('download-progress', (progressObj) => {
+	  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+	  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+	  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+	  sendStatusToWindow(log_message);
+	})
+	
+	function sendStatusToWindow(text) {
+	  win.webContents.send('message', text);
+	}
+	
+	ipcRenderer.on('message', function(event, text) {
+	 var process_container = document.getElementById('messages');
+	 var update_details = document.getElementById('check');
+	 update_details.innerHTML = text;
+	 
+	 $scope.updateDetails = text;
+	}) */
 
 });
