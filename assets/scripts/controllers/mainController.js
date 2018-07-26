f1d39ab8d80a3b97d3a9a6b39bc19c2d3b773546
Jeet <Jeet@Metapercept.com>
2018-07-26 17:27:35 +0530

    0.6.6

--- assets/scripts/controllers/mainController.js
+++ assets/scripts/controllers/mainController.js
@@ -1,6 +1,30 @@
-appName.controller('mainController', function ($scope) {
+appName.controller('mainController', function ($scope, electron, ipc) {
+
+const ipcRenderer = require('electron').ipcRenderer;
 
 	$scope.appVersion = require('electron').remote.app.getVersion();
 	console.log($scope.appVersion);
 
+	ipcRenderer.on('updateAvailable', function(event, text) {
+			var update = document.querySelector("#notify");
+			update.innerHTML = "New Updates Available";
+			update.opened = true;
+    })
+	ipcRenderer.on('updateNotAvailable', function(event, text) {
+			var no_update = document.querySelector("#notify");
+			no_update.innerHTML = "Latest Version";
+			no_update.opened = true;
+    })	
+	ipcRenderer.on('updateReady', function(event, text) { 
+           $timeout(function() {
+			angular.element('#update_available').triggerHandler('click');
+			});
+    })
+	
+	var notification = document.querySelector("#notify");
+	$scope.installUpdates = function(){
+		ipcRenderer.send('quitAndInstall');
+	}
+	
+		
 });
